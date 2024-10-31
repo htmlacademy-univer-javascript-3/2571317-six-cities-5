f2559ff4e23337b178902cc117b13e947d336691
Anatoly Kopyl <hi@kopyl.dev>
2024-10-31 23:58:32 +0300

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
+import ProtectedRoute from './components/protected-route';
+import Favorites from './pages/favorites';
 import Home from './pages/home';
+import Login from './pages/login';
+import Offer from './pages/offer';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>,
+  },
+  {
+    path: '/login',
+    element: <Login/>
+  },
+  {
+    path: '/offer/:id',
+    element: <Offer/>
+  },
+  {
+    path: '/favorites',
+    element: <ProtectedRoute user={null}>
+      <Favorites/>
+    </ProtectedRoute>
+  }
+]);
 
 function App() {
-  return <Home placeAmount={1} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,7 @@
 import PlaceCard, {TPlaceProps} from './place-card.tsx';
 import {PlaceType} from '../../enums.ts';
 
-type THomeProps = {
-  placeAmount: number;
-}
-function Home(props: THomeProps) {
+function Home() {
   const places: (TPlaceProps & {key: string})[] = [
     {
       name: 'Sample',
